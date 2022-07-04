837c3e5682fa581039b0fd2c09d3fa4458dd0427
Agustina Valentin <valentinagustina3@gmail.com>
2022-07-03 23:23:08 -0300

    ruteo

--- routes/nosotros.js
+++ routes/nosotros.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+    res.render( 'nosotros' ,{
+      isNosotros: true
+    });//vamos a necesitar en view/nosotros.hbs
+  });
 
 module.exports = router;

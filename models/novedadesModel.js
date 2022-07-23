2699e983877130eef1faaff3f9f422c4752f0356
Agustina Valentin <valentinagustina3@gmail.com>
2022-07-23 15:10:00 -0300

    modificar novedades

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -23,12 +23,25 @@ async function deleteNovedadByID(id){
     var rows = await pool.query(query, [id]);
     return rows;
 }
-
+/*traer la novedad dentro del form*/
 async function getNovedadesByID(id){
     var query= 'select * from novedades where id=?';
-    var rows = await pool.query(query, [id]);
-    return rows;
+    var rows = await pool.query(query,[id]);
+    return rows [0];
 
 }
 
-module.exports = {getNovedades, insertNovedades, deleteNovedadByID, getNovedadesByID}
+async function modificarNovedadByID(obj,id){
+    try{
+    var query= 'update novedades set ? where id=?';
+    var row = await pool.query(query,[obj,id]);
+    return row;
+}catch(error){
+    throw error;
+}
+
+}
+
+
+
+module.exports = {getNovedades, insertNovedades, deleteNovedadByID, getNovedadesByID,modificarNovedadByID}
